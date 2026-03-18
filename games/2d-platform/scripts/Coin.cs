94fb3b6b06d8ea93ae0509fe0459bd1e39faec7d
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2026-03-18 14:47:48 -0400

    collect coin

---
+++ games/2d-platform/scripts/Coin.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Coin : Area2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	
+	// This is the method the Signal is looking for
+	private void _on_body_entered(Node2D body)
+	{
+		GD.Print("Coin collected by: " + body.Name);
+		
+		// Optional: Remove the coin when touched
+		QueueFree(); 
+	}
+}
