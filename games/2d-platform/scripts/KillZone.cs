b598ffff52e7cf1e0562397d553d98ff383766ba
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2026-03-19 17:26:01 -0400

    death effect

--- games/2d-platform/scripts/KillZone.cs
+++ games/2d-platform/scripts/KillZone.cs
@@ -3,21 +3,49 @@ using System;
 
 public partial class KillZone : Area2D
 {
+	[Export] private ColorRect _deathOverlay; 
 	private Timer _timer;
+
 
 	public override void _Ready()
 	{
 		_timer = GetNode<Timer>("Timer");
+		ColorRect sceneOverlay = GetNodeOrNull<ColorRect>("CanvasLayer/ColorRect");
+		if (_deathOverlay == null && sceneOverlay != null)
+		{
+			_deathOverlay = sceneOverlay;
+		}
+		if (_deathOverlay != null) _deathOverlay.Visible = false;
 	}
 
 	private void _on_body_entered(Node2D body)
 	{
 		GD.Print("You Died!");
+		
+
+		// Use GetNodeOrNull to prevent crashing if the node is missing
+		var collisionShape = body.GetNodeOrNull<CollisionShape2D>("CollisionShape2D");
+		if (collisionShape != null)
+		{
+			collisionShape.QueueFree();
+		}
+
+		if (_deathOverlay != null)
+			{
+				_deathOverlay.Visible = true;
+				var tween = CreateTween();
+				_deathOverlay.Color = new Color(0, 0, 0, 0); // Start transparent
+				tween.TweenProperty(_deathOverlay, "color", new Color(0, 0, 0, 1), 0.5f)
+					 .SetTrans(Tween.TransitionType.Quint);
+			}
+
+		Engine.TimeScale = 0.5f;
 		_timer.Start(); 
 	}
 
 	private void _on_timer_timeout() 
 	{
+		Engine.TimeScale = 1.0f;
 		GetTree().ReloadCurrentScene();
 	}
 }
