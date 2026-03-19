b9705f484926bc3c3ecc53b07c368c9e77338aca
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2026-03-19 17:50:51 -0400

    music and sound effects

--- games/2d-platform/scripts/Coin.cs
+++ games/2d-platform/scripts/Coin.cs
@@ -3,9 +3,12 @@ using System;
 
 public partial class Coin : Area2D
 {
+	private AnimationPlayer _AnimationPlayer;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_AnimationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,8 +19,6 @@ public partial class Coin : Area2D
 	private void _on_body_entered(Node2D body)
 	{
 		GD.Print("Coin collected by: " + body.Name);
-		
-		// Remove the coin when touched
-		QueueFree(); 
+		_AnimationPlayer.Play("pickup");
 	}
 }
